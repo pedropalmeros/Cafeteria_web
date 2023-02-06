20e427c6a38166324af6b5b909004e87fed1a590
pedropalmeros <pedrofpalmeros@gmail.com>
2023-02-05 22:43:02 -0600

    Adding gulp to the project and the first task to compile a scss file

---
+++ gulpfile.js
@@ -0,0 +1,16 @@
+const {src, dest} = require('gulp'); 
+const sass = require('gulp-sass')(require('sass'));
+
+
+function css(done){
+    //compilar sass
+    // pasos: 1 Identificar archivo, 2- Compilar, 3- Guardad el .css
+    src("./src/scss/app.scss")
+        .pipe( sass() )
+        .pipe( dest('build/css'));
+    
+    done();
+
+}
+
+exports.css = css;
