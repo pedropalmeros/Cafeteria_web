079300315977429ed7a55d52736e6f8b8b5bcc0c
pedropalmeros <pedrofpalmeros@gmail.com>
2023-02-05 23:04:40 -0600

    Adding postcss and autoprefixer to the compiling task from gulp

--- gulpfile.js
+++ gulpfile.js
@@ -1,12 +1,15 @@
 const {src, dest, watch} = require('gulp'); 
 const sass = require('gulp-sass')(require('sass'));
+const postcss = require('gulp-postcss');
+const autoprefixer = require('autoprefixer');
 
 
 function css(done){
     //compilar sass
     // pasos: 1 Identificar archivo, 2- Compilar, 3- Guardad el .css
     src("./src/scss/app.scss")
-        .pipe( sass() )
+        .pipe( sass({ outputStyle: 'compressed'}) )
+        .pipe(postcss([autoprefixer()]))
         .pipe( dest('build/css'));
     
     done();
