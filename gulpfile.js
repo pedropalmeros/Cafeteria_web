b01721fdefcbdaf98a317b2caf756b823f4d67eb
pedropalmeros <pedrofpalmeros@gmail.com>
2023-02-09 05:37:56 -0600

    adding mixins for mediaquery

--- gulpfile.js
+++ gulpfile.js
@@ -7,6 +7,7 @@ const autoprefixer = require('autoprefixer');
 
 // IMAGENES
 const imagemin = require('gulp-imagemin');
+const webp = require('gulp-webp');
 
 
 function css(done){
@@ -40,10 +41,18 @@ function imagenes(done){
 //    done();
 //}
 
+function versionWebp(){
+    const opciones = { quality: 50 }
+    return src('./src/img/**/*.{png,jpg}')
+        .pipe(webp(opciones))
+        .pipe(dest('build/img'))
+}
+
 
 
 
 exports.css = css;
 exports.dev = dev;
 exports.imagenes = imagenes;
-exports.default = series( imagenes, css, dev);
+exports.versionWebp = versionWebp
+exports.default = series( imagenes, versionWebp, css, dev);
