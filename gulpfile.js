628b6fb3e52d139863adb1f76811a8634e526187
pedropalmeros <pedrofpalmeros@gmail.com>
2023-02-08 23:42:14 -0600

    adding header html

--- gulpfile.js
+++ gulpfile.js
@@ -8,7 +8,8 @@ function css(done){
     //compilar sass
     // pasos: 1 Identificar archivo, 2- Compilar, 3- Guardad el .css
     src("./src/scss/app.scss")
-        .pipe( sass({ outputStyle: 'compressed'}) )
+        .pipe( sass() )
+        //.pipe( sass({ outputStyle: 'compressed'}) )
         .pipe(postcss([autoprefixer()]))
         .pipe( dest('build/css'));
     
